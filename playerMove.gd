1df2174dce71a3d07f8943d7a425b65d8649fc41
whitzy <54811854+itswhitzy@users.noreply.github.com>
2022-11-26 21:25:58 +0300

    goodbye

--- playerMove.gd
+++ playerMove.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 # Movement speed in pixels per second.
 export var speed := 500
 
+var bullet = preload("res://Bullet.tscn")
+var bullet_speed = 700
+
 # We map a direction to a frame index of our AnimatedSprite node's sprite frames.
 # See how we use it below to update the character's look direction in the game.
 var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
@@ -21,6 +24,16 @@ func _physics_process(_delta: float) -> void:
 		direction = direction.normalized()
 	move_and_slide(speed * direction)
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child", bullet_instance)
 
 
 
