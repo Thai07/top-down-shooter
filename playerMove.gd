d03e56e3a8b73b5f1755b471b8e357d8304f0a6b
Thai <61286182+Thai07@users.noreply.github.com>
2022-11-26 20:57:10 +0300

    haraket bitti

---
+++ playerMove.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+# Movement speed in pixels per second.
+export var speed := 500
+
+# We map a direction to a frame index of our AnimatedSprite node's sprite frames.
+# See how we use it below to update the character's look direction in the game.
+var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
+var _velocity := Vector2.ZERO
+
+onready var animated_sprite: AnimatedSprite = $AnimatedSprite
+
+
+func _physics_process(_delta: float) -> void:
+	var direction := Vector2 (
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	move_and_slide(speed * direction)
+
+
+
+
+
+
+
